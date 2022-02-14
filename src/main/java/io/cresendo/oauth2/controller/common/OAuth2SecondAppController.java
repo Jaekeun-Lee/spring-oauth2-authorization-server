6f18f28561a3d233cc3612f92fb60b4e36a3fec5
이재근 <ljg960730@etoos.com>
2022-02-14 16:46:22 +0900

    token refresh & second App client

--- src/main/java/io/cresendo/oauth2/controller/common/OAuth2SecondAppController.java
+++ src/main/java/io/cresendo/oauth2/controller/common/OAuth2SecondAppController.java
@@ -18,15 +18,15 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/oauth2")
-public class OAuth2Controller {
+public class OAuth2SecondAppController {
 
     private final Gson gson;
     private final RestTemplate restTemplate;
 
-    @GetMapping("/callback")
-    public OAuthToken callbackSocial(@RequestParam String refreshToken) {
+    @GetMapping("/secondapp/callback")
+    public OAuthToken callbackSocial(@RequestParam String code) {
 
-        String credentials = "testClientId:testSecret";
+        String credentials = "secondapp:secondsecret";
         String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
 
         HttpHeaders headers = new HttpHeaders();
@@ -35,11 +35,11 @@ public class OAuth2Controller {
         headers.add("Authorization", "Basic " + encodedCredentials);
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("refresh_token", refreshToken);
-        params.add("grant_type", "refresh_token");
-//        params.add("code", code);
-//        params.add("grant_type", "authorization_code");
-//        params.add("redirect_uri", "http://localhost:8081/oauth2/callback");
+//        params.add("refresh_token", refreshToken);
+//        params.add("grant_type", "refresh_token");
+        params.add("code", code);
+        params.add("grant_type", "authorization_code");
+        params.add("redirect_uri", "http://localhost:8081/oauth2/secondapp/callback");
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
         ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/oauth/token", request, String.class);
 
@@ -49,10 +49,10 @@ public class OAuth2Controller {
         return null;
     }
 
-    @GetMapping(value = "/token/refresh")
+    @GetMapping(value = "/secondapp/token/refresh")
     public OAuthToken refreshToken(@RequestParam String refreshToken) {
 
-        String credentials = "testClientId:testSecret";
+        String credentials = "secondapp:secondsecret";
         String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
 
         HttpHeaders headers = new HttpHeaders();
